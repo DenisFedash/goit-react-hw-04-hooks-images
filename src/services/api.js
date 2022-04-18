b9a1282f867fdd274e1eb52c1828dae62ccc9fe6
Denis Fedash <0501054104d@gmail.com>
2022-04-18 23:11:19 +0300

    fix API

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,10 @@
 const URL = 'https://pixabay.com/api';
 const KEY = '25267969-1b21c6cce0210f4f694534b04';
 
-function fetchImages(query, page = 1) {
+const fetchImages = (query, page = 1) => {
   return fetch(
-    `${URL}?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    `${URL}/?q=${query}&page=${page}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`
   ).then(response => response.json());
-}
+};
 
 export default fetchImages;
